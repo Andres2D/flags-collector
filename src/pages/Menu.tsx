e7af266fd6503dc0ee4940e4e99a84f032c8cdc9
Andres2D <andres2d1997.1206@gmail.com>
2025-01-29 23:05:06 -0500

    feat: add cards for game modes

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -24,6 +24,101 @@ function Menu() {
           onClick={() => navigate('/guess-by-name')}  
         >Random</button>
       </div>
+
+      <br /><br /><br /><br />
+
+      <h2 className='gamemode-title'>Select game mode</h2>
+        <section className="gamemode-cards">
+          <div className="gamemode-card">
+            <div className="gmc-section1">
+              <h4>Guess by Name</h4>
+              <a href="$">Play</a>
+            </div>
+            <div className="gmc-section2">
+              <div className="gmcs-text1">
+                <h5>Rules</h5>
+                <p>Re-usable components built using Radir UI and Tailwind CSS</p>
+              </div>
+
+              <div className="gmcs-text2">
+                <h5>Difficulty</h5>
+                <p>How to install dependencies and structure your app.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="gamemode-card">
+            <div className="gmc-section1">
+              <h4>Guess by Flag</h4>
+              <a href="$">Play</a>
+            </div>
+            <div className="gmc-section2">
+              <div className="gmcs-text1">
+                <h5>Rules</h5>
+                <p>Re-usable components built using Radir UI and Tailwind CSS</p>
+              </div>
+
+              <div className="gmcs-text2">
+                <h5>Difficulty</h5>
+                <p>How to install dependencies and structure your app.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="gamemode-card">
+            <div className="gmc-section1">
+              <h4>Color by Flag</h4>
+              <a href="$">Play</a>
+            </div>
+            <div className="gmc-section2">
+              <div className="gmcs-text1">
+                <h5>Rules</h5>
+                <p>Re-usable components built using Radir UI and Tailwind CSS</p>
+              </div>
+
+              <div className="gmcs-text2">
+                <h5>Difficulty</h5>
+                <p>How to install dependencies and structure your app.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="gamemode-card">
+            <div className="gmc-section1">
+              <h4>Guess by Map</h4>
+              <a href="$">Play</a>
+            </div>
+            <div className="gmc-section2">
+              <div className="gmcs-text1">
+                <h5>Rules</h5>
+                <p>Re-usable components built using Radir UI and Tailwind CSS</p>
+              </div>
+
+              <div className="gmcs-text2">
+                <h5>Difficulty</h5>
+                <p>How to install dependencies and structure your app.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="gamemode-card">
+            <div className="gmc-section1">
+              <h4>Random</h4>
+              <a href="$">Play</a>
+            </div>
+            <div className="gmc-section2">
+              <div className="gmcs-text1">
+                <h5>Rules</h5>
+                <p>Re-usable components built using Radir UI and Tailwind CSS</p>
+              </div>
+
+              <div className="gmcs-text2">
+                <h5>Difficulty</h5>
+                <p>How to install dependencies and structure your app.</p>
+              </div>
+            </div>
+          </div>
+        </section>
     </section>
   ) 
 }
